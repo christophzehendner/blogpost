c4e3b9897e8ec385290cac49341db9067b619545
rshdd <raschidllamas@hotmail.com>
2021-10-27 12:39:17 -0500

    Make it better

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const methodOverride = require('method-override')
 const app = express()
 
 const { MongoClient } = require('mongodb');
-//const uri = "mongodb+srv://rshdd:hola123@blogpostup.mwjnm.mongodb.net/posts?retryWrites=true&w=majority";
-const uri = process.env.MONGODB_URI;
+const uri = "mongodb+srv://rshdd:hola123@blogpostup.mwjnm.mongodb.net/posts?retryWrites=true&w=majority";
+//const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
+client.connect((err) => {
   console.log("Connected");
   const database = client.db("posts");
   // perform actions on the collection object
