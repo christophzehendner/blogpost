46e04bcc981a8d8b580578b40498f1246b88c641
rshdd <raschidllamas@hotmail.com>
2021-10-26 19:33:29 -0500

    Make it better

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
+const app = express();
 
-mongoose.connect('mongodb://localhost/blog', {
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://rshdd:rshdd@blogpostup.mwjnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://rshdd:<password>@blogpostup.mwjnm.mongodb.net/admin?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+/*mongoose.connect('mongodb://localhost/blog', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
@@ -13,20 +31,31 @@ mongoose.connect('mongodb://localhost/blog', {
   console.log("DB Connected!")
 }).catch(err => {
   console.log(Error, err.message);
-})
+})*/
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 app.use('/public', express.static('public'));
 
 app.get('/', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' })
   res.render('articles/index', { articles: articles })
-})
+});
 
 app.use('/articles', articleRouter)
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
+mongoose.Promise = global.Promise;
+app.use(bodyParser.json())
+// middleware para admitir errores:
+app.use((err,req,res, next) => {
+  res.status(422).send({
+    error: err.message
+  })
+})
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
