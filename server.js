4e77317f7bbfce98a307f193676e8846c753b003
rshdd <raschidllamas@hotmail.com>
2021-10-27 18:51:26 -0500

    Make it better

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ client.connect((err) => {
 });*/
 
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://rshdd:hola@blogpostup.mwjnm.mongodb.net/test?retryWrites=true";
+const uri = "rshdd:hola@blogpostup.mwjnm.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect((err) => {
+client.connect(err => {
   const collection = client.db("test");
   // perform actions on the collection object
   client.close();
